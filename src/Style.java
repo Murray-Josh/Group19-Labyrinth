7cc30f69befa83051f4275da1331fa257eb69167
Aaron <aarondavies135@gmail.com>
2020-11-14 14:19:13 +0000

    Rough style class

--- src/Style.java
+++ src/Style.java
@@ -1,11 +1,20 @@
-//I kept getting popups about there being a style class in javafx as well >:(
+import javax.swing.text.html.ImageView;
 
 /**
  *
  *
- * @author Aaron Davis and Isabelle Ludwig
+ * @author Aaron Davies and Isabelle Ludwig
  * @version 1.0
  */
 public class Style {
+    public PirateImages(){
+        ImageView PiratePlayer1Icon = new ImageView("patrick.jpg")
+        ImageView PirateCornerPiece = new ImageView("patrick.jpg")
+    }
+    public void getPlayer1Icon(){
 
+    }
+    public void setPlayer1Icon(){
+
+    }
 }
