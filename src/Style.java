11820f59e5f59c485e82a69d5d1cd32a5205500d
issiludwig <isabelle.freya.ludwig@web.de>
2020-11-14 13:55:08 +0000

    javadoc

--- src/Style.java
+++ src/Style.java
@@ -1,4 +1,11 @@
 //I kept getting popups about there being a style class in javafx as well >:(
+
+/**
+ *
+ *
+ * @author Aaron Davis and Isabelle Ludwig
+ * @version 1.0
+ */
 public class Style {
 
 }
