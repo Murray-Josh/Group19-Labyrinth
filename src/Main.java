60e622ac39ebfbb2880feac982ef6c5e115c74f9
joshua <j.murray567@gmail.com>
2020-11-18 18:24:34 +0000

    Commit of main class to produce game board

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
+import java.util.ArrayList;
+
 public class Main {
 
 
     public static void main(String[] args) {
-        // write your code here
+        Style style = new Style();
+        ArrayList<Player> players = new ArrayList<>();
+        Gameboard gameboard = new Gameboard(new int[]{10, 10}, style, players);
+        gameboard.boardToString();
     }
 }
