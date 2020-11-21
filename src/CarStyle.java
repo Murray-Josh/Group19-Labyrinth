e79670a401f86ef02fdbf38dceecef602298c469
issiludwig <isabelle.freya.ludwig@web.de>
2020-11-21 22:57:18 +0000

    split up style into three classes and added images into file

--- src/CarStyle.java
+++ src/CarStyle.java
@@ -1,24 +1,6 @@
 import javafx.scene.image.Image;
 
-import java.io.Serializable;
-
-/**
- * Gives the Images for the Car Style
- *
- * @author Aaron Davies, Isabelle Ludwig & Joseph Omar
- * @version 1.0
- */
-public class CarStyle extends Style implements Serializable {
-    static {
-        PLAYER_ONE = new Image("patrick.jpg");
-        PLAYER_TWO = new Image("patrick.jpg");
-        PLAYER_THREE = new Image("patrick.jpg");
-        PLAYER_FOUR = new Image("patrick.jpg");
-        STRAIGHT_TILE = new Image("patrick.jpg");
-        CORNER_TILE = new Image("patrick.jpg");
-        JUNCTION_TILE = new Image("patrick.jpg");
-        GAMEBOARD = new Image("patrick.jpg");
-public class CarStyle {
+public class CarStyle extends Style{
     public static void CarsImages() {
         private static final Image Cars_PLAYER_ONE = new Image("patrick.jpg");
         private static final Image Cars_PLAYER_TWO = new Image("patrick.jpg");
--- src/MouseTrapStyle.java
+++ src/MouseTrapStyle.java
@@ -1,22 +1,43 @@
 import javafx.scene.image.Image;
 
 import java.io.Serializable;
+public class MouseTrapStyle extends Style{
+    public static void MouseTrapImages() {
+        private static final Image MouseTrap_PLAYER_ONE = new Image("patrick.jpg");
+        private static final Image MouseTrap_PLAYER_TWO = new Image("patrick.jpg");
+        private static final Image MouseTrap_PLAYER_THREE = new Image("patrick.jpg");
+        private static final Image MouseTrap_PLAYER_FOUR = new Image("patrick.jpg");
+        private static final Image MouseTrap_STRAIGHT_TILE = new Image("patrick.jpg");
+        private static final Image MouseTrap_CORNER_TILE = new Image("patrick.jpg");
+        private static final Image MouseTrap_JUNCTION_TILE = new Image("patrick.jpg");
+        private static final Image MouseTrap_GAMEBOARD_TILE = new Image("patrick.jpg");
+    }
+    public Image getCORNER_TILE(){
+        return CORNER_TILE;
+    }
+    public Image getJUNCTION_TILE(){
+        return JUNCTION_TILE;
+    }
+    public Image getGOAL_TILE(){
+        return GOAL_TILE;
+    }
+    public Image getGAMEBOARD_BORDER(){
+        return GAMEBOARD_BORDER;
+    }
 
-/**
- * Gives the Images for the Mouse Trap Style
- *
- * @author Aaron Davies, Isabelle Ludwig & Joseph Omar
- * @version 1.0
- */
-public class MouseTrapStyle extends Style implements Serializable {
-    static {
-        PLAYER_ONE = new Image("patrick.jpg");
-        PLAYER_TWO = new Image("patrick.jpg");
-        PLAYER_THREE = new Image("patrick.jpg");
-        PLAYER_FOUR = new Image("patrick.jpg");
-        STRAIGHT_TILE = new Image("patrick.jpg");
-        CORNER_TILE = new Image("patrick.jpg");
-        JUNCTION_TILE = new Image("patrick.jpg");
-        GAMEBOARD = new Image("patrick.jpg");
+    public Image getPLAYER_ONE(){
+        return PLAYER_ONE;
+    }
+
+    public Image getPLAYER_TWO(){
+        return PLAYER_TWO;
+    }
+
+    public Image getPLAYER_THREE(){
+        return PLAYER_THREE;
+    }
+
+    public Image getPLAYER_FOUR(){
+        return PLAYER_FOUR;
     }
 }
