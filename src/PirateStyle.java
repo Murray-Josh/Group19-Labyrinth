d3dfb67bd9a24ff644f611f2c741a60b8a89e4c0
issiludwig <isabelle.freya.ludwig@web.de>
2020-11-23 11:07:22 +0000

    added some graphics

--- src/PirateStyle.java
+++ src/PirateStyle.java
@@ -10,19 +10,19 @@ import java.io.Serializable;
  */
 public class PirateStyle extends Style implements Serializable {
     static {
-        PLAYER_ONE = new Image("patrick.jpg");
-        PLAYER_TWO = new Image("patrick.jpg");
-        PLAYER_THREE = new Image("patrick.jpg");
-        PLAYER_FOUR = new Image("patrick.jpg");
-        STRAIGHT_TILE = new Image("cars_straight.png");
-        STRAIGHT_FIRE = new Image("cars_straight_fire.png");
-        STRAIGHT_ICE = new Image("cars_straight_ice.png");
-        CORNER_TILE = new Image("cars_corner.png");
-        CORNER_FIRE = new Image("cars_corner_fire.png");
-        CORNER_ICE = new Image("cars_corner_ice.png");
-        JUNCTION_TILE = new Image("cars_junction.png");
-        JUNCTION_FIRE = new Image("cars_junction_fire.png");
-        JUNCTION_ICE = new Image("cars_junction_ice.png");
+        PLAYER_ONE = new Image("pirate_player_one.png");
+        PLAYER_TWO = new Image("pirate_player_two.png");
+        PLAYER_THREE = new Image("pirate_player_three.png");
+        PLAYER_FOUR = new Image("pirate_player_four.png");
+        STRAIGHT_TILE = new Image("pirate_straight.png");
+        STRAIGHT_FIRE = new Image("pirate_fire.png");
+        STRAIGHT_ICE = new Image("pirate_ice.png");
+        CORNER_TILE = new Image("pirate_corner.png");
+        CORNER_FIRE = new Image("pirate_fire.png");
+        CORNER_ICE = new Image("pirate_ice.png");
+        JUNCTION_TILE = new Image("pirate_junction.png");
+        JUNCTION_FIRE = new Image("pirate_fire.png");
+        JUNCTION_ICE = new Image("pirate_ice.png");
         GAMEBOARD = new Image("patrick.jpg");
     }
 }
