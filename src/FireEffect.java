a2c9ef3d1393070cf51afc7ed2fb1b73e13660f1
WalidMohamedCS <61463255+WalidMohamedCS@users.noreply.github.com>
2020-11-23 11:10:59 +0000

    Create FireEffect.java

---
+++ src/FireEffect.java
@@ -0,0 +1,15 @@
+public class FireEffect extends Effect.TileEffect {
+
+    protected FireEffect(boolean onFire) {
+        super(onFire);
+    }
+    public boolean isOnFire() {
+        return this.onFire;
+    }
+
+    public void setOnFire(boolean onFire) {
+        this.onFire = onFire;
+        System.out.print("This tile is on fire");
+    }
+
+}
