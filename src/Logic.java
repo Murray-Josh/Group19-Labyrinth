bfd1a751905da9b010789697ed1f10034a0febb5
joshua <j.murray567@gmail.com>
2020-11-23 13:51:16 +0000

    Created Logic.java

---
+++ src/Logic.java
@@ -0,0 +1,28 @@
+public class Logic {
+
+
+    public void StartGame(){
+
+    }
+
+    public void NextPlayer(){
+
+    }
+
+    public void DrawTile(){
+
+    }
+
+    public void PlaceTile(){
+
+    }
+
+    public void PlayEffect(){
+
+    }
+
+    public void Move(){
+
+    }
+
+}
