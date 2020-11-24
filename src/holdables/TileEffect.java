8b2df0906a59cefa847562ebba55fae1c9f24bc6
WalidMohamedCS <61463255+WalidMohamedCS@users.noreply.github.com>
2020-11-24 11:50:31 +0000

    Create TileEffect.java

---
+++ src/holdables/TileEffect.java
@@ -0,0 +1,9 @@
+public class TileEffect extends Effect {
+
+    public TileEffect(boolean onFire, boolean isFrozen) {
+        super(onFire, isFrozen);
+    }
+
+    public TileEffect(boolean isFrozen) {
+    }
+}
