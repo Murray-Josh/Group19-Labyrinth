2c9a515819f7f1d9f18cff32ea180d849ea8f3c6
MartinioS <martinsamm20@gmail.com>
2020-11-24 12:03:42 +0000

    Tried to make a gameboard, does not work yet

--- old/Main.java
+++ old/Main.java
@@ -2,6 +2,7 @@ package core;
 
 import holdables.Tile;
 import players.Player;
+import styles.CarStyle;
 import styles.PirateStyle;
 import styles.Style;
 
@@ -16,5 +17,12 @@ public class Main {
         Gameboard gameboard = new Gameboard(new int[]{10, 10}, style, players);
         gameboard.boardToString();
         System.out.println(Tile.class.getName());
+    }
+    private static void makeGameboard() {
+        Style style = new CarStyle();
+        ArrayList<Player> players = new ArrayList<>();
+        Gameboard gameboard = new Gameboard(new int[]{2, 2}, style, players);
+        Tile tile = new Tile (new int[]{2, 2}, 2, style, 90, True);
+
     }
 }
