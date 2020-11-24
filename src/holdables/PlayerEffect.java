962511ca5e64e0f52ceca95d9b3628e8957941e9
WalidMohamedCS <61463255+WalidMohamedCS@users.noreply.github.com>
2020-11-24 11:49:52 +0000

    Create PlayerEffect.java

---
+++ src/holdables/PlayerEffect.java
@@ -0,0 +1,9 @@
+public class PlayerEffect extends Effect {
+
+    public PlayerEffect(boolean doubleMovement, boolean backMovement) {
+        super(doubleMovement, backMovement);
+    }
+
+    public PlayerEffect(boolean doubleMovement) {
+    }
+}
