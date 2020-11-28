ff6223c608c0f6296aa4d0e40c318a25bf543c84
MartinioS <martinsamm20@gmail.com>
2020-11-28 12:25:32 +0000

    Style - Added a constructor as a superclass GameBoardTest - made a static variable called 'c' on line 22 and got each image from it (lines 24-26) CarStyle - Added a constructor as a subclass (done the same for Pirate and MouseTrap)

--- old/Main.java
+++ old/Main.java
@@ -2,7 +2,6 @@ package core;
 
 import holdables.Tile;
 import players.Player;
-import styles.CarStyle;
 import styles.PirateStyle;
 import styles.Style;
 
@@ -17,12 +16,5 @@ public class Main {
         Gameboard gameboard = new Gameboard(new int[]{10, 10}, style, players);
         gameboard.boardToString();
         System.out.println(Tile.class.getName());
-    }
-    private static void makeGameboard() {
-        Style style = new CarStyle();
-        ArrayList<Player> players = new ArrayList<>();
-        Gameboard gameboard = new Gameboard(new int[]{2, 2}, style, players);
-        Tile tile = new Tile (new int[]{2, 2}, 2, style, 90, True);
-
     }
 }
