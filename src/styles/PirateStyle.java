4dceca9c4e77f59ac25463dfe68ca47d6359193e
issiludwig <isabelle.freya.ludwig@web.de>
2020-11-28 13:23:14 +0000

    added new pirate ice graphics

--- src/styles/PirateStyle.java
+++ src/styles/PirateStyle.java
@@ -18,13 +18,13 @@ public final class PirateStyle extends Style implements Serializable {
         PLAYER_FOUR = new Image("pirate_player_four.png");
         STRAIGHT_TILE = new Image("pirate_straight.png");
         STRAIGHT_FIRE = new Image("pirate_fire.png");
-        STRAIGHT_ICE = new Image("pirate_ice.png");
+        STRAIGHT_ICE = new Image("pirate_straight_ice.png");
         CORNER_TILE = new Image("pirate_corner.png");
         CORNER_FIRE = new Image("pirate_fire.png");
-        CORNER_ICE = new Image("pirate_ice.png");
+        CORNER_ICE = new Image("pirate_corner_ice.png");
         JUNCTION_TILE = new Image("pirate_junction.png");
         JUNCTION_FIRE = new Image("pirate_fire.png");
-        JUNCTION_ICE = new Image("pirate_ice.png");
+        JUNCTION_ICE = new Image("pirate_junction_ice.png");
         GAMEBOARD = new Image("pirate_boarder.png ");
     }
     public PirateStyle() {
