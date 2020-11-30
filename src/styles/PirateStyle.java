642a69cb9866f1297a9f54937ee50d29e432a62f
jomar0 <51296266+jomar0@users.noreply.github.com>
2020-11-30 21:14:58 +0000

    Reworked Styles, though stuff should still work. I hope

--- src/styles/PirateStyle.java
+++ src/styles/PirateStyle.java
@@ -39,7 +39,7 @@ public enum PirateStyle implements Serializable, Style {
      */
     @Override
     public Image getPlayerOne() {
-        return null;
+        return PLAYER_ONE.IMAGE;
     }
 
     /**
