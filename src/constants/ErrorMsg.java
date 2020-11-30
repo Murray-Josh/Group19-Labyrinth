9fee683d6a3a5b44797de89a36c42b077f8361db
jomar0 <51296266+jomar0@users.noreply.github.com>
2020-11-30 09:00:47 +0000

    changes error msg file paths, adjusted format settings and reformatted

--- src/constants/ErrorMsg.java
+++ src/constants/ErrorMsg.java
@@ -8,24 +8,24 @@ package constants;
  */
 public enum ErrorMsg {
     FXML_NOT_FOUND("The FXML file for this window cannot be found.\nPlease verify game files and directories.\n",
-            "FXML Not Found"),
+                   "FXML Not Found"),
+    MESSGAGE_FAIL("The Message of the Day could not be fetched", "Message of the Day Error"),
     PROFILE_ADD_ERROR("That nickname is already taken, please choose a new one\n", "Cannot Create Profile"),
-    PROFILE_FILE_NOT_FOUND("The profiles save file <profiles.txt> could not be found.\nPlease verify that the file is" +
-            " in the saves folder.\n", "Profiles Not Found"),
-    PROFILE_LOAD_ERROR("The profiles save file <profiles.txt> could not be parsed\n", "Profile Parse Error"),
+    PROFILE_FILE_NOT_FOUND("The profiles save file <profiles.ser> could not be found.\nPlease verify that the file is" +
+                                   " in the saves folder.\n", "Profiles Not Found"),
+    PROFILE_LOAD_ERROR("The profiles save file <profiles.ser> could not be parsed\n", "Profile Parse Error"),
     PROFILE_NOT_SELECTED("You need to select at least two profiles", "Not Enough Profiles Selected"),
     PROFILE_NOT_VALID("You must select a profile", "Selected Profile Not Valid"),
-    PROFILE_READ_ERROR("The profiles save file <profiles.txt> could not be read.\nPlease verify all game files.\n",
-            "Profiles Read Error"),
-    PROFILE_WRITE_ERROR("The profiles save file <profiles.txt> could not be written.\nPlease verify all game files " +
-            "and directories.\n", "Profiles Write Error"),
+    PROFILE_READ_ERROR("The profiles save file <profiles.ser> could not be read.\nPlease verify all game files.\n",
+                       "Profiles Read Error"),
+    PROFILE_WRITE_ERROR("The profiles save file <profiles.ser> could not be written.\nPlease verify all game files " +
+                                "and directories.\n", "Profiles Write Error"),
     SAVE_FILE_NOT_FOUND("The savegame selected could not be found\n", "Savegame File Path Error"),
     SAVE_LOAD_ERROR("The savegame selected could not be loaded\n", "Savegame Load Error"),
     SAVE_NOT_SELECTED("You have not selected a save file to load\n", "No Savegame Selected"),
     SAVE_READ_ERROR("The savegame selected could not be parsed\n", "Savegame Parse Error"),
     SAVE_WRITE_ERROR("The game could not be written to a file\n", "Savegame Write Error"),
-    STYLE_NOT_VALID("The selected Style is unavailable", "Style Unavailable"),
-    MESSGAGE_FAIL("The Message of the Day could not be fetched", "Message of the Day Error");
+    STYLE_NOT_VALID("The selected Style is unavailable", "Style Unavailable");
 
     private final String MESSAGE;
     private final String HEADER;
