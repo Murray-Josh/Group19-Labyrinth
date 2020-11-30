a870e5ed55feec8f1e6d71db2f487b90aa5fc2cc
jomar0 <51296266+jomar0@users.noreply.github.com>
2020-11-30 14:34:16 +0000

    peepee

---
+++ src/controllers/GameboardController.java
@@ -0,0 +1,16 @@
+package controllers;
+
+public class GameboardController {
+    @FXML
+    private GridPane grdBoard;
+    @FXML
+    private VBox vboxPlayers;
+    @FXML
+    private VBox vboxEffects;
+    @FXML
+    private Button cmdActivate;
+    @FXML
+    private Label lblStatus;
+    @FXML
+    private Button cmdSilkBag;
+}
