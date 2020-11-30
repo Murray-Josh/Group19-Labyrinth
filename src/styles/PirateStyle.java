f1a304aa6557ee33382e7acd2c314d1b8c4ec3ec
MartinioS <martinsamm20@gmail.com>
2020-11-30 01:49:59 +0000

    Added comments to FindMessages and added Goal Tile image to the PirateStyle class (wasn't there previously)

--- src/styles/PirateStyle.java
+++ src/styles/PirateStyle.java
@@ -25,6 +25,7 @@ public final class PirateStyle extends Style implements Serializable {
         JUNCTION_TILE = new Image("resources/styles/pirate/pirate_junction.png");
         JUNCTION_FIRE = new Image("resources/styles/pirate/pirate_fire.png");
         JUNCTION_ICE = new Image("resources/styles/pirate/pirate_junction_ice.png");
+        GOAL_TILE = new Image("resources/styles/pirate/pirate_goal.png");
         GAMEBOARD = new Image("resources/styles/pirate/pirate_boarder.png");
     }
 
