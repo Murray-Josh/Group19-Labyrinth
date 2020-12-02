79c8e1054f896b125bce8c28cea792206d0142a0
WalidMohamedCS <61463255+WalidMohamedCS@users.noreply.github.com>
2020-12-02 12:02:06 +0000

    just planning ahead before I code later

--- src/players/dontwork/TurnCounter.java
+++ src/players/dontwork/TurnCounter.java
@@ -1,7 +1,7 @@
 public class TurnCounter {
 
     private boolean playerOnePlaysNext;
-    private P[] players;
+    protected P[] players;
     //playerOnePlaysNext = true;
 
     public void switchPlayer() {
@@ -29,8 +29,35 @@ public class TurnCounter {
             switchPlayer();
             System.out.println("It is Player Two's turn");
         }
+   /*     if (currentPlayer == playerOne)
+        {
+            currentPlayer = playerTwo;
+        }
+        else if (currentPlayer == playerTwo)
+        {
+            currentPlayer = playerOne;
+        }
     }
 
     private void reset() {
     }
 }
+*/
+        
+    /* initialize
+    List<Player> players = Arrays.asList(new Player(...),new Player(...));
+        Player currentPlayer = players.get(0);
+
+
+        // switch Player:
+        currentPlayer = players.get(1-players.indexOf(currentPlayer));
+
+        // version for more than two players:
+        // select the next player from list and restart with the first after the last:
+        currentPlayer = players.get((players.indexOf(currentPlayer)+1)%players.size());
+
+        // similar by cycling the list
+        currentPlayer = players.remove(0); // get the next player from list
+        players.add(currentPlayer); // put it at the end of the list */
+
+// how to implement a player losing their turn due to freeze or inability to move due to tile shifts.
