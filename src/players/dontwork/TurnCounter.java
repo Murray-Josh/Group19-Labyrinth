6e4716e6eb66fd10e5077f3118be51cdee0b4953
WalidMohamedCS <61463255+WalidMohamedCS@users.noreply.github.com>
2020-12-03 04:43:19 +0000

    Update TurnCounter.java

--- src/players/dontwork/TurnCounter.java
+++ src/players/dontwork/TurnCounter.java
@@ -1,17 +1,36 @@
+/**
+ * TurnCounter.java
+ *
+ * @author Walid Mohamed
+ * @version 1.0.0
+ */
 public class TurnCounter {
 
     private boolean playerOnePlaysNext;
+    private boolean playerTwoPlaysNext;
+    private boolean playerThreePlaysNext;
+    private boolean playerFourPlaysNext;
+
     protected P[] players;
     //playerOnePlaysNext = true;
 
     public void switchPlayer() {
 
-        boolean onePlaysNext;
+        boolean playerRotation;
         if (!playerOnePlaysNext) playerOnePlaysNext = true;
         else playerOnePlaysNext = false;
+        if (playerTwoPlaysNext) {
+            playerTwoPlaysNext = false;
+            playerThreePlaysNext = !playerThreePlaysNext;
+            playerFourPlaysNext = !playerFourPlaysNext;
+        } else {
+            playerTwoPlaysNext = true;
+            playerThreePlaysNext = !playerThreePlaysNext;
+            playerFourPlaysNext = !playerFourPlaysNext;
+        }
     }
 
-    public TurnCounter(P... players) {
+    protected TurnCounter(P... players) {
         this.players = players;
         reset();
 
@@ -29,35 +48,23 @@ public class TurnCounter {
             switchPlayer();
             System.out.println("It is Player Two's turn");
         }
-   /*     if (currentPlayer == playerOne)
-        {
-            currentPlayer = playerTwo;
+        //Player 3:
+        if (!playerTwoPlaysNext) {
+
+            //player 3 acts.
+            switchPlayer();
+            System.out.println("It is Player Three's turn");
         }
-        else if (currentPlayer == playerTwo)
-        {
-            currentPlayer = playerOne;
+        //Player 4:
+        if (!playerThreePlaysNext) {
+
+            //player 4 acts.
+            switchPlayer();
+            System.out.println("It is Player Four's turn");
         }
     }
 
     private void reset() {
+
     }
 }
-*/
-        
-    /* initialize
-    List<Player> players = Arrays.asList(new Player(...),new Player(...));
-        Player currentPlayer = players.get(0);
-
-
-        // switch Player:
-        currentPlayer = players.get(1-players.indexOf(currentPlayer));
-
-        // version for more than two players:
-        // select the next player from list and restart with the first after the last:
-        currentPlayer = players.get((players.indexOf(currentPlayer)+1)%players.size());
-
-        // similar by cycling the list
-        currentPlayer = players.remove(0); // get the next player from list
-        players.add(currentPlayer); // put it at the end of the list */
-
-// how to implement a player losing their turn due to freeze or inability to move due to tile shifts.
